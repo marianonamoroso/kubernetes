a1bb1b0cda415b38450fb9faaf0ded14762090e6
Mariano N. Amoroso <marianonamoroso@gmail.com>
2021-08-03 02:15:42 -0300

    Add files via upload

---
+++ pod-logging.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# POD ERRORS LOGGING
+# AUTHOR: MARIANO AMOROSO
+# BIN: kubectl
+# NAMESPACE: emed
+
+for pod in $(kubectl get pods -n emed --no-headers=true | awk '$serviceapi {print $1}')
+do
+	tput sgr0 ; echo "Extracting logs of: $pod" 
+	date >$pod.logs
+	echo "################################################">>$pod.logs
+	echo "POD_NAME:$pod">>$pod.logs
+       	echo "################################################">>$pod.logs
+	kubectl logs $pod -n emed >>$pod.logs
+	tput setaf 2 ;echo "A log file was created: $pod.logs"
+	echo ""
+done
