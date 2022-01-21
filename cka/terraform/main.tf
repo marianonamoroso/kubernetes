76922cdfa28d495ea06c54066b18336f4ed9af0c
Mariano N. Amoroso <marianonamoroso@gmail.com>
2022-01-20 23:30:44 -0300

    Create main.tf

---
+++ cka/terraform/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "node" {
+  for_each               = local.ec2
+
+  ami                    = each.value.ami
+  instance_type          = each.value.instance_type
+  vpc_security_group_ids = each.value.instance_type == "t2.medium" ? [aws_security_group.master-sg.id] : [aws_security_group.worker-sg.id]
+
+  tags = {
+    deployment  = "TF"
+    cert        = "CKA"
+  }
+}
