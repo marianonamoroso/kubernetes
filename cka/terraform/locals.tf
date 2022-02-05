6a79f5d9a3c9016316218c35fc728851954589bb
Mariano N Amoroso <marianonamoroso@Marianos-MacBook-Pro.local>
2022-02-05 12:11:09 -0300

    enhancing the code

--- cka/terraform/locals.tf
+++ cka/terraform/locals.tf
@@ -14,7 +14,7 @@ locals {
 	    /etc/init.d/hostname.sh start
     USERDATA
 
-     #user_data for worker-2 node
+    #user_data for worker-2 node
         worker-2 = <<USERDATA
         #!/bin/bash
         hostnamectl set-hostname worker-2
@@ -23,10 +23,11 @@ locals {
     }
 
     ec2 = {
-        master-node   = {
-        ami           = "ami-04505e74c0741db8d"
-        instance_type = "t2.medium"
-        user_data     = base64encode(format(local.user_data["master"]))
+        master-node = {
+        ami                    = "ami-04505e74c0741db8d"
+        instance_type          = "t2.medium"
+        user_data              = base64encode(format(local.user_data["master"]))
+        vpc_security_group_ids = [aws_security_group.master-sg.id]
 
         tags = {
                 Name        = "master-node"
@@ -34,28 +35,35 @@ locals {
                 Cert        = "CKA"
                 Author      = "MA"
             }  
+
         }
         worker-node-1 = {
-        ami           = "ami-04505e74c0741db8d"
-        instance_type = "t2.large"
-        user_data     = base64encode(format(local.user_data["worker-1"]))
+        ami                    = "ami-04505e74c0741db8d"
+        instance_type          = "t2.large"
+        user_data              = base64encode(format(local.user_data["worker-1"]))
+        vpc_security_group_ids = [aws_security_group.worker-sg.id]
+
         tags = {
                 Name        = "worker-node-1"
                 Deployment  = "TF"
                 Cert        = "CKA"
                 Author      = "MA"
             }
+
         }
         worker-node-2 = {
-        ami           = "ami-04505e74c0741db8d"
-        instance_type = "t2.large"
-        user_data     = base64encode(format(local.user_data["worker-2"]))
+        ami                    = "ami-04505e74c0741db8d"
+        instance_type          = "t2.large"
+        user_data              = base64encode(format(local.user_data["worker-2"]))
+        vpc_security_group_ids = [aws_security_group.worker-sg.id]
+
         tags = {
                 Name        = "worker-node-2"
                 Deployment  = "TF"
                 Cert        = "CKA"
                 Author      = "MA"
             }
+            
         }
     }
 }    
