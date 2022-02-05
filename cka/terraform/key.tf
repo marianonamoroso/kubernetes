19b5390d873bbba6e2f9edfd4db1a4d2ad385693
Mariano N Amoroso <marianonamoroso@Marianos-MacBook-Pro.local>
2022-02-05 11:59:52 -0300

    new features

--- cka/terraform/key.tf
+++ cka/terraform/key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "k8s-key" {
-  key_name   = "k8s-node"
-  public_key = ""
+  key_name   = "k8s-ssh"
+  public_key = "<YOUR_SSH_PUBLIC_KEY>" # ssh-keygen -y -f key.pem > key.pub
 }
