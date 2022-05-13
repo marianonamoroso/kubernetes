4a91169dd29205ebb669319a4ea6fd87885d0e98
Mariano N Amoroso <marianonamoroso@Marianos-MacBook-Pro.local>
2022-05-13 10:50:50 -0300

    Fix Files

--- cka/terraform/key.tf
+++ cka/terraform/key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "k8s-key" {
   key_name   = "k8s-ssh"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCWRZIl2tB4qrryuggWIy388oPg7e5d4YDcw7r6J8D/TCvBNUtKlsO7Wseb5GxwB4HbUSuuXU/kvjZbu593kee1H6pq00PXW28sIDepdzYY/HYUI6azX2jO1kuWg6Fy6MaqOvzj68vhfOSnfwzTY4TyTJJr1lJ0BHNATA9bSzZZrjQDWnYPsp/qvwzV4YZ4GjfOSaovEYH4lgbi2/jfwqXDLsbYFVXTtwsVn34Rly1oiUGE8dxQxyG6GAlLJ/tryMAm+Z5Yzni/dGFiIO0n4/eC9o2lYKXtRtzTo3G1z6UALrfywUVLWpYPrOV8MwpL+n5zDzR4ctTzqVjdhaayd5cL" # ssh-keygen -y -f key.pem > key.pub
+  public_key = "<YOUR_KEY>" # ssh-keygen -y -f key.pem > key.pub
 }
--- cka/terraform/provider.tf
+++ cka/terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA3YAB62G4DBNL4CVI"
-  secret_key = "8TjFVjVKAzNmP6HBa/VaPR6hQtt9S06KLGZQYCyB"
+  access_key = "<YOUR_ACCES_KEY>"
+  secret_key = "<YOUR_SECRET>"
 }
